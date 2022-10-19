b8157a98dbf800a76cd888019c929557cd035105
Chris Guikema <chrisguikema@dornerworks.com>
2022-10-19 15:25:17 -0400

    virtio_emul: clean cache after read/write
    This commit adds a cache clean when dealing with the guest memory
    space. The cache operations slow down the virtio throughput, but can
    fix issues with guest virtio processes.
    
    Signed-off-by: Chris Guikema <chris.guikema@dornerworks.com>

--- libsel4vmmplatsupport/src/drivers/virtio_emul_helpers.c
+++ libsel4vmmplatsupport/src/drivers/virtio_emul_helpers.c
@@ -7,12 +7,26 @@
 #include <sel4vm/guest_vm.h>
 #include <sel4vm/guest_ram.h>
 
+#include <sel4vmmplatsupport/gen_config.h>
+
 #include "virtio_emul_helpers.h"
 
 static int read_guest_mem(vm_t *vm, uintptr_t phys, void *vaddr, size_t size, size_t offset, void *cookie)
 {
+#ifdef CONFIG_VMM_PLATSUPPORT_VIRTIO_CLEAN_CACHE
+    seL4_CPtr cap = vspace_get_cap(&vm->mem.vmm_vspace, vaddr);
+    if (cap == seL4_CapNull) {
+        /* Not sure how we would get here, something has gone pretty wrong */
+        ZF_LOGE("Failed to get vmm cap for vaddr: %p", vaddr);
+        return -1;
+    }
+    int error = seL4_ARM_Page_CleanInvalidate_Data(cap, 0, size);
+    ZF_LOGF_IF(error, "seL4_ARM_Page_CleanInvalidate_Data failed");
+#endif
+
     /* Copy memory from the guest (vaddr) to our given memory destination (cookie) */
     memcpy(cookie + offset, vaddr, size);
+
     return 0;
 }
 
@@ -20,6 +34,18 @@ static int write_guest_mem(vm_t *vm, uintptr_t phys, void *vaddr, size_t size, s
 {
     /* Copy memory to our guest (vaddr) from our given memory location (cookie) */
     memcpy(vaddr, cookie + offset, size);
+
+#ifdef CONFIG_VMM_PLATSUPPORT_VIRTIO_CLEAN_CACHE
+    seL4_CPtr cap = vspace_get_cap(&vm->mem.vmm_vspace, vaddr);
+    if (cap == seL4_CapNull) {
+        /* Not sure how we would get here, something has gone pretty wrong */
+        ZF_LOGE("Failed to get vmm cap for vaddr: %p", vaddr);
+        return -1;
+    }
+    int error = seL4_ARM_Page_CleanInvalidate_Data(cap, 0, size);
+    ZF_LOGF_IF(error, "seL4_ARM_Page_CleanInvalidate_Data failed");
+#endif
+
     return 0;
 }
 
